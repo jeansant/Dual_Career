5d208395c2d38d544e7dcf2568b4c8711b2435e5
Jean <jeansant@buffalo.edu>
2019-06-03 21:57:00 -0400

    Initial Commit

---
+++ Dual_Career/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dual_Career
+//
+//  Created by Jean Santiuste on 6/3/19.
+//  Copyright © 2019 Jean Santiuste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
