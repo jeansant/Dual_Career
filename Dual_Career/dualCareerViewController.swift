5e9f091a00e4e8a08bf1f7655c03f3af7f5cfae8
Jean <jeansant@buffalo.edu>
2019-06-04 20:43:30 -0400

    Incorporated checkmark functionality and accessories for tableview cells.

--- Dual_Career/dualCareerViewController.swift
+++ Dual_Career/dualCareerViewController.swift
@@ -17,7 +17,42 @@ class dualCareerViewController: UITableViewController {
         super.viewDidLoad()
 
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection
+        section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath:
+        IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dualCareerCell",
+            for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        //print(itemArray[indexPath.row])
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType = .disclosureIndicator
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+  
+        // checkmark functionality
+        /*
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+ */
+    }
 
 }
 
