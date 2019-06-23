507b6fef21b3acfb74af15211fd0e2edfaa02514
Jean <jeansant@buffalo.edu>
2019-06-23 17:21:02 -0400

    Checkmark functionality complete and appending to to do list array.

--- Dual_Career/Controllers/dualCareerViewController.swift
+++ Dual_Career/Controllers/dualCareerViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class dualCareerViewController: UITableViewController {
     
-    let itemArray = ["Career 1", "Career 2", "Personal"]
+    var itemArray = [Item]()
     
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Shake Booty"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Do Homework"
+        itemArray.append(newItem3)
+        
+        
+        
+        if let items = defaults.array(forKey: "DualCareerArray") as? [Item] {
+            itemArray = items
+        }
 
     }
     
@@ -29,7 +48,14 @@ class dualCareerViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "dualCareerCell",
             for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Ternary operator ==>
+        // value = condition ? valueIfTrue : valueIfFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
         
@@ -39,7 +65,11 @@ class dualCareerViewController: UITableViewController {
     {
         //print(itemArray[indexPath.row])
         
-        tableView.cellForRow(at: indexPath)?.accessoryType = .disclosureIndicator
+        //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
   
@@ -64,13 +94,24 @@ class dualCareerViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Career", style: .default) { (action) in
             // what will happen once the user clicks the add career button on UIAlert
-            print("Complete")
+           
+            let newitem = Item()
+            
+            newitem.title = textField.text!
+            
+            self.itemArray.append(newitem)
+            
+            self.defaults.set(self.itemArray, forKey: "DualCareerArray")
+            
+            self.tableView.reloadData()
         }
+        
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Add a new career"
             textField = alertTextField
             
         }
+        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
---
+++ Dual_Career/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Dual_Career
+//
+//  Created by Jean Santiuste on 6/23/19.
+//  Copyright © 2019 Jean Santiuste. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
